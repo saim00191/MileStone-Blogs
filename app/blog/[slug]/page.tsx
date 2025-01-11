cc84668abc708709265a8e875b5b0fffb4f29d48
Muhammad-Talha103 <talhagp908@gmail.com>
2025-01-11 16:46:07 +0500

    define types

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,8 +2,8 @@ import { client } from "@/sanity/lib/client";
 import Blog from "./Blog";
 
 const BlogPage = async ({ params }: { params: { slug: string } }) => {
-
-  const { slug } = await params;  
+ 
+  const { slug } = params;  
 
   const query = `*[_type == "blog" && slug.current == $slug]{ 
     title,
@@ -14,14 +14,13 @@ const BlogPage = async ({ params }: { params: { slug: string } }) => {
     content
   }`;
 
-
   const blogPost = await client.fetch(query, { slug });
+
 
   if (!blogPost || blogPost.length === 0) {
     return <div>Blog post not found.</div>;
   }
 
- 
   return <Blog blogPost={blogPost[0]} slug={slug} />;
 };
 
